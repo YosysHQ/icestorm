f7cb6e8e07c9b314e5052c4b0416db422dbdeb7b
Clifford Wolf <clifford@clifford.at>
2015-12-08 12:54:20 +0100

    Added icestick and hx8kboard examples

---
+++ examples/hx8kboard/example.v
@@ -0,0 +1,25 @@
+module top (
+	input  clk,
+	output LED0,
+	output LED1,
+	output LED2,
+	output LED3,
+	output LED4,
+	output LED5,
+	output LED6,
+	output LED7
+);
+
+	localparam BITS = 8;
+	localparam LOG2DELAY = 22;
+
+	reg [BITS+LOG2DELAY-1:0] counter = 0;
+	reg [BITS-1:0] outcnt;
+
+	always@(posedge clk) begin
+		counter <= counter + 1;
+		outcnt <= counter >> LOG2DELAY;
+	end
+	
+	assign {LED0, LED1, LED2, LED3, LED4, LED5, LED6, LED7} = outcnt ^ (outcnt >> 1);
+endmodule
