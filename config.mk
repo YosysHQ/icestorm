43e77416362f57e4dbc3ac7cb8049ab3a427130f
Clifford Wolf <clifford@clifford.at>
2016-02-29 10:37:42 +0100

    Use pkg-config for libftdi

--- config.mk
+++ config.mk
@@ -1,9 +1,11 @@
 CXX = clang
 CC = $(CXX)
+PKG_CONFIG = pkg-config
 DESTDIR ?=
 PREFIX ?= /usr/local
 
 ifeq ($(MXE),1)
 EXE = .exe
 CXX = /usr/local/src/mxe/usr/bin/i686-pc-mingw32-gcc
+PKG_CONFIG = /usr/local/src/mxe/usr/bin/i686-pc-mingw32-pkg-config
 endif
