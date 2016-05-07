af36a8ade3f69d7b5513bd5593559416c8a2d833
Clifford Wolf <clifford@clifford.at>
2016-05-07 10:55:34 +0200

    Fixes for MXE build

--- config.mk
+++ config.mk
@@ -6,6 +6,6 @@ PREFIX ?= /usr/local
 
 ifeq ($(MXE),1)
 EXE = .exe
-CXX = /usr/local/src/mxe/usr/bin/i686-pc-mingw32-gcc
-PKG_CONFIG = /usr/local/src/mxe/usr/bin/i686-pc-mingw32-pkg-config
+CXX = /usr/local/src/mxe/usr/bin/i686-w64-mingw32.static-gcc
+PKG_CONFIG = /usr/local/src/mxe/usr/bin/i686-w64-mingw32.static-pkg-config
 endif
