426d82dce0c4a140147c32b671ff7a5da8af12e9
Udi Finkelstein <github@udifink.com>
2017-01-01 01:03:27 +0200

    Fix Windows builds

--- config.mk
+++ config.mk
@@ -8,5 +8,6 @@ CHIPDB_SUBDIR ?= icebox
 ifeq ($(MXE),1)
 EXE = .exe
 CXX = /usr/local/src/mxe/usr/bin/i686-w64-mingw32.static-gcc
+CC ?= $(CXX)
 PKG_CONFIG = /usr/local/src/mxe/usr/bin/i686-w64-mingw32.static-pkg-config
 endif
