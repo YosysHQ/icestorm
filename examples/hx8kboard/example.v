f920831e43c686dcaeca39c2481d5f22c014940f
Clifford Wolf <clifford@clifford.at>
2017-01-09 21:21:15 +0100

    Some cleanups in verilog examples

--- examples/hx8kboard/example.v
+++ examples/hx8kboard/example.v
@@ -16,7 +16,7 @@ module top (
 	reg [BITS+LOG2DELAY-1:0] counter = 0;
 	reg [BITS-1:0] outcnt;
 
-	always@(posedge clk) begin
+	always @(posedge clk) begin
 		counter <= counter + 1;
 		outcnt <= counter >> LOG2DELAY;
 	end
