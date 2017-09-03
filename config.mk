b87a750f4b7f06c31bbb6ab70c62104f1cc67f5a
Marcus Müller <marcus@hostalia.de>
2017-09-03 19:20:50 +0200

    modularized standard Makefile config

--- config.mk
+++ config.mk
@@ -1,11 +1,19 @@
+PREFIX ?= /usr/local
+
 CXX ?= clang++
 CC ?= clang
-LDLIBS = -lm -lstdc++
-CFLAGS += -MD -O0 -ggdb -Wall -std=c99 -I/usr/local/include
-CXXFLAGS += -MD -O0 -ggdb -Wall -std=c++11 -I/usr/local/include
 PKG_CONFIG ?= pkg-config
+
+C_STD ?= c99
+CXX_STD ?= c++11
+OPT_LEVEL ?= 0
+WARN_LEVEL ?= all
+
+LDLIBS = -lm -lstdc++
+CFLAGS += -MD -O$(OPT_LEVEL) -ggdb -W$(WARN_LEVEL) -std=$(C_STD) -I$(PREFIX)/include
+CXXFLAGS += -MD -O$(OPT_LEVEL) -ggdb -W$(WARN_LEVEL) -std=$(CXX_STD) -I$(PREFIX)/include
+
 DESTDIR ?=
-PREFIX ?= /usr/local
 CHIPDB_SUBDIR ?= icebox
 
 ifeq ($(MXE),1)
