a34ef88b8e8142a8a6b88c8e14c36c828fe51493
Andrew Wygle <awygle@gmail.com>
2018-05-12 14:15:24 -0700

    Added missing ieren entries for lm4k.
    Config SPI pins weren't present in ioctrl_lm4k.sh

--- icefuzz/tests/ioctrl_lm4k.sh
+++ icefuzz/tests/ioctrl_lm4k.sh
@@ -10,9 +10,9 @@ pins="
   B1 B2    B4    B6 B7
   C1    C3 C4    C6 C7
   D1 D2 D3       D6 D7
-  E1 E2 E3 E4 E5    E7
-     F2 F3 F4       F7
-        G3
+  E1 E2 E3 E4 E5 E6 E7
+     F2 F3 F4 F5 F6 F7
+        G3       G6
 "
 
 pins="$( echo $pins )"
