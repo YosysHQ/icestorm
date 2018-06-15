YosysHQ/icestorm

commit f7e9fec63a3f88bee8c27e858da319ea03d68d14

Allow prefixes in multiple chained statements.
IE
```
    lutff_1 {
        lutff_1/out -> local_g2_1 -> lutff_1/in_0
        local_g2_2 -> lutff_1/in_3
        local_g2_7 -> lutff_1/in_2
    }
```

icebox/icebox_hlc2asc.py
@@ -803,7 +803,11 @@ class LogicCell:
             self.seq_bits[2] = '1'
         elif fields == ['async_setreset']:
             self.seq_bits[3] = '1'
-        elif len(fields) >= 3 and (fields[1] == '->' or fields[1] == '<->'):
+        elif len(fields) > 3 and (fields[1] == '->' or fields[1] == '<->'):
+            self.read(fields[:3])
+            self.read(fields[2:])
+            return
+        elif len(fields) == 3 and (fields[1] == '->' or fields[1] == '<->'):
             prefix = 'lutff_%d/' % self.index
 
             # Strip prefix if it is given
@@ -960,7 +964,10 @@ class IOBlock:
                           == ("padin_glb_netwk", fields[2][10:])]
             assert len(bit) == 1
             self.tile.ic.extra_bits.add(bit[0])
-        elif len(fields) >= 3 and (fields[1] == '->' or fields[1] == '<->'):
+        elif len(fields) > 3 and (fields[1] == '->' or fields[1] == '<->'):
+            self.read(fields[:3])
+            self.read(fields[2:])
+        elif len(fields) == 3 and (fields[1] == '->' or fields[1] == '<->'):
             prefix = 'io_%d/' % self.index
 
             # Strip prefix if it is given

commit fdc6f1a1b11f9c163bc120cae2c313a77d1f25bf

icebox: Fix issue introduced by f7e9fec63a3f88bee8c27e858da319ea03d68d14
Current code fails with the following;
```
Parse error in line 94:
        span4_y3_g15_6 -> local_g0_3 -> D_OUT_0
```

icebox/icebox_hlc2asc.py
@@ -871,7 +871,7 @@ class LogicCell:
             elif fields[-1].startswith('in_'):
                 self.tile.read(fields[:-1] + [prefix + fields[-1]])
             else:
-                raise ParseError
+                self.tile.read(fields)
             return
 
         bits = ''.join([
@@ -1037,7 +1037,7 @@ class IOBlock:
                                 'OUT_ENB'):
                 self.tile.read(fields[:-1] + [prefix + fields[-1]])
             else:
-                raise ParseError
+                self.tile.read(fields)
         else:
             raise ParseError
 